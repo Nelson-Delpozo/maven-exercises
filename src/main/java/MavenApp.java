db8a07c452fc3dd16b409a08bddb69be12bf4366
Nelson Delpozo <nelson.delpozo99@gmail.com>
2022-03-08 15:21:48 -0600

    finished dependencies

--- src/main/java/MavenApp.java
+++ src/main/java/MavenApp.java
@@ -1,14 +1,29 @@
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.Scanner;
+
 import static org.apache.commons.lang3.StringUtils.capitalize;
+import static org.apache.commons.lang3.StringUtils.reverse;
 
 public class MavenApp {
 
     public static void main(String[] args) {
         System.out.println("hello from maven project");
+        Scanner scannyhead = new Scanner(System.in);
+        System.out.print("enter a string: ");
+        String test = scannyhead.nextLine();
 
-        String test = "hello";
+        String capitalize = capitalize(test);
+        System.out.println("capitalize = " + capitalize);
 
-        String newTest = capitalize(test);
-        System.out.println(newTest);
+        String reversed = reverse(test);
+        System.out.println("reversed = " + reversed);
+
+        String swapCase = StringUtils.swapCase(test);
+        System.out.println("swapCase = " + swapCase);
+
+        boolean isAlpha = StringUtils.isAlphaSpace(test);
+        System.out.println("isAlpha = " + isAlpha);
 
     }
 
