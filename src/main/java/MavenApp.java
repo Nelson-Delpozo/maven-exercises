2058bcfd3ad49fa6757037ecc4c39f4e7c78eb2d
Nelson Delpozo <nelson.delpozo99@gmail.com>
2022-03-08 15:12:58 -0600

    dependencies

--- src/main/java/MavenApp.java
+++ src/main/java/MavenApp.java
@@ -1,12 +1,17 @@
+import static org.apache.commons.lang3.StringUtils.capitalize;
+
 public class MavenApp {
 
     public static void main(String[] args) {
+        System.out.println("hello from maven project");
 
+        String test = "hello";
 
-
-
+        String newTest = capitalize(test);
+        System.out.println(newTest);
 
     }
+
 
 
 }
